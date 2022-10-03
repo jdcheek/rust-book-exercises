713c1e10087913f03d67bc20c64e23653e820dcf
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-10-02 20:05:02 -0500

    end of variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // mutable variables with let mut
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    // constants with const, uppercase snake case convention 
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // shadowing
+    let y = 5;
+    let y = y + 1;
+    { 
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}"); 
+    }
+    println!("The value of y is: {x}");
+
 }
