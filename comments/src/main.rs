0143e935d6e27a74c8869610f2bfcfefb2b06a0d
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-10-03 22:05:11 -0500

    comments

---
+++ comments/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // single line comments
+    let num = 7; // lucky number
+
+    // convention is the comment separate line above topic
+    
+    /* 
+    Multi-line comments are not listed in this section. 
+    I found a github issue #693 from several years ago stating this is on purpose
+    because multi-line comments are not idiomatic according to steveklabnik.
+    So this is how you do it... 
+    */
+}
